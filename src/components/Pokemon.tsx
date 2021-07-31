7b15aa9eddabf11c316ae7ed750cecc43b104c35
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2021-07-31 22:08:28 +0100

    Display more information in pokemon component

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,15 +1,30 @@
 import { forwardRef } from "react";
+import { getPokemonImageUrl } from "api/pokemon-api";
 
 type Props = {
+  id: number;
   name: string;
+  onClick?: (id: number) => void;
 };
 
 export default forwardRef(function Pokemon(props: Props, ref: any) {
-  const { name } = props;
+  const { id, name, onClick } = props;
+
+  function onItemClickHandler() {
+    if (typeof onClick === "function") {
+      onClick(id);
+    }
+  }
 
   return (
-    <div ref={ref} className="shadow-md py-2 px-4">
-      <span>{name}</span>
+    <div
+      onClick={onItemClickHandler}
+      ref={ref}
+      className="shadow-md py-2 px-4 cursor-pointer hover:bg-gray-100"
+    >
+      <h1 className="text-black text-center font-medium">{name}</h1>
+      <img className="w-full" alt={name} src={getPokemonImageUrl(id)} />
+      <div className="flex-grow" />
     </div>
   );
 });
