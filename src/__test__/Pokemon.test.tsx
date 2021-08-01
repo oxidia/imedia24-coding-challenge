ab578286dede1faa0ce250311c518d29c77e9067
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2021-08-01 01:07:14 +0100

    Write Pokemon component unit tests

--- src/__test__/Pokemon.test.tsx
+++ src/__test__/Pokemon.test.tsx
@@ -1,14 +1,41 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Pokemon from "components/Pokemon";
+import { useState } from "react";
+
+function MockPokemon(props: { id: number }) {
+  const [id, setId] = useState<number>(-1);
+
+  return (
+    <div>
+      <Pokemon id={props.id} name="bulbasaur" onClick={setId} />
+      <span>{id}</span>
+    </div>
+  );
+}
 
 describe("Pokemon component", () => {
-  it("renders hello world", () => {
+  it("Should display pokemon name", () => {
     const name = "bulbasaur";
 
     render(<Pokemon id={1} name={name} />);
 
-    const spanElement = screen.getByText(name);
+    const headingElement = screen.getByText(name);
 
-    expect(spanElement).toBeInTheDocument();
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  it("Should display pokemon id onClick", () => {
+    const name = "bulbasaur";
+    const id = 1;
+
+    render(<MockPokemon id={id} />);
+
+    const headingElement = screen.getByText(name);
+
+    fireEvent.click(headingElement);
+
+    const spanElement = screen.getByText(id);
+
+    expect(spanElement.textContent).toBe(id.toString());
   });
 });
--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -17,14 +17,13 @@ export default forwardRef(function Pokemon(props: Props, ref: any) {
   }
 
   return (
-    <div
+    <section
       onClick={onItemClickHandler}
       ref={ref}
       className="shadow-md py-2 px-4 cursor-pointer hover:bg-gray-100"
     >
-      <h1 className="text-black text-center font-medium">{name}</h1>
+      <h1 className="text-black text-center font-medium py-2">{name}</h1>
       <img className="w-full" alt={name} src={getPokemonImageUrl(id)} />
-      <div className="flex-grow" />
-    </div>
+    </section>
   );
 });
