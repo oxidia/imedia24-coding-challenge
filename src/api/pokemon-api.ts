459e460419568c4e9d132224ae2a4448d559c16e
menoiy <karim.daou.1993@gmail.com>
2021-10-20 16:31:55 +0100

    Feat: add keyword options to getPokemonsList and create new function getPokemonsByKeyword

--- src/api/pokemon-api.ts
+++ src/api/pokemon-api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 type GetPokemonsListParams = {
   limit?: number;
   offset?: number;
+  keyword?: string;
 };
 
 axios.defaults.baseURL = "https://pokeapi.co/api/v2";
@@ -14,6 +15,11 @@ export async function getPokemonsList(
   const limit = options?.limit ?? 10;
   const offset = options?.offset ?? 0;
 
+  console.log("ok => ", options?.keyword);
+  if (options?.keyword) {
+    return getPokemonsByKeyword({ ...options, limit, offset });
+  }
+
   const { data } = await axios.get("/pokemon", {
     params: {
       limit: limit,
@@ -21,6 +27,16 @@ export async function getPokemonsList(
     }
   });
   return data;
+}
+
+export async function getPokemonsByKeyword(
+  options: GetPokemonsListParams
+): Promise<any> {
+  const { data } = await axios.get("/pokemon");
+
+  return data
+    .filter((pokemon: any) => pokemon.name.starWith(options!.keyword))
+    .slice(options!.offset, options!.limit);
 }
 
 export async function getPokemonById(id: number): Promise<any> {
